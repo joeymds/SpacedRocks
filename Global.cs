d546dc226bbf1b99dcbfc6fc26a6a71b5c648523
joey <joey.mds@gmail.com>
2020-06-10 14:45:17 +0100

    added global.cs

---
+++ Global.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public class Global : Node2D
+{
+
+    public int ShieldStrength { get; set; }
+    public int PlayerScore { get; set; }
+    
+    public override void _Ready()
+    {
+        AddToGroup("Global");
+        PlayerScore = 0;
+        ShieldStrength = 100;
+    }
+
+    public void UpdateShield(int updateValue)
+    {
+        ShieldStrength += updateValue;
+        if (ShieldStrength > 100)
+            ShieldStrength = 100;
+    }
+
+    public void UpdateScore(int updateValue)
+    {
+        PlayerScore = updateValue;
+    }
+
+}
